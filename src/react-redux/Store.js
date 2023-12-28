5aaefb5cdd6ea83390098851701fc0a393cf1de0
Mani <118741380+Manikanta662001@users.noreply.github.com>
2023-12-28 07:10:58 +0530

    Update Store.js

--- src/react-redux/Store.js
+++ src/react-redux/Store.js
@@ -1,12 +1,23 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore,getDefaultMiddleware} from '@reduxjs/toolkit'
+import {combineReducers} from 'redux'
 import { Reducer } from './Reducer';
+import {usersReducer} from './usersReducer';
 import Cakereducer from './Cakereducer';
+import thunkMiddleware from 'redux-thunk';
 
+
+const rootReducer = combineReducers({
+    counter : Reducer,
+    cakes:Cakereducer,
+    usersReducer
+})
 const Store = configureStore({
-    reducer:{
-        counter : Reducer,
-        cakes:Cakereducer
-
-    }
+   reducer:rootReducer,
+   middleware:[
+    //this is default middleware
+    // ...getDefaultMiddleware(),
+    thunkMiddleware
+],
+   devTools:process.env.NODE_ENV !== 'production',
 })
 export default Store;
